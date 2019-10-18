bd647fc5fb6cd947fed6a26ff5df4328f1c2b8a5
Colin Maloney <colinmaloney@gmail.com>
2019-10-17 22:14:03 -0700

    model_finising

--- flask/car_matching.py
+++ flask/car_matching.py
@@ -1,17 +1,25 @@
 import flask
 from flask import request, jsonify
+from flask_restful import Resource, Api
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+api = Api(app)
 
-
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Time to Match your Car</h1>    <p>This site is a prototype API.</p>"
-
+class IntroToWebsite(Resource):
+    def get(self):
+        return "<h1>Time to Match your Car</h1>    <p>This site is a prototype API.</p>"
+class CarSelection(Resource):
+    def get(self):
+        return "<h1>Pick your car from the drop down list</h1>"
+# @app.route('/', methods=['GET'])
+# def drop_down_list():
+#     return "{% for each in listStatus %} <option value="{{each}}" {% if each == "list_status" %} selected {% endif %}>{{each}}</option> {% endfor %}"
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+api.add_resource(IntroToWebsite,'/')
+api.add_resource(CarSelection,'/car_matching')
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -25,7 +33,7 @@ if __name__ == "__main__":
 # {
 #   "embeddings": [
 #     {
-#       "tensorName": "My tensor",
+#       "tensorName": "Car Nodes",
 #       "tensorShape": [
 #         1000,
 #         50
